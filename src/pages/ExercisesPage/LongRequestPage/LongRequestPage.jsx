3cf0dcc4e48807ca1c5c37fda0708124dc6334fc
imykhailychenko <igor.c.m@ukr.net>
2023-01-27 08:36:20 +0100

    q-and-a

--- src/pages/ExercisesPage/LongRequestPage/LongRequestPage.jsx
+++ src/pages/ExercisesPage/LongRequestPage/LongRequestPage.jsx
@@ -1,41 +1,113 @@
-import { Component } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
-class LongRequestPage extends Component {
-  state = {
-    isError: false,
-    isLoading: false,
-    isDone: false,
-  };
+import axios from 'axios';
 
-  async componentDidMount() {
-    this.setState({ isLoading: true, isError: false });
+const CODE = 'ERR_CANCELED';
 
-    // TODO fetch https://goit-fs.netlify.app/api/long
-    // if ok      -> this.setState({ isDone: true, isLoading: false });
-    // if error   -> this.setState({ isError: true, isLoading: false });
-  }
+const LongRequestPage = () => {
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isDone, setIsDone] = useState(false);
 
-  componentWillUnmount() {
-    // TODO
-  }
+  const controllerRef = useRef(new AbortController());
 
-  render() {
-    const { isError, isLoading, isDone } = this.state;
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        await axios.get('https://goit-fs.netlify.app/api/long', {
+          signal: controllerRef.current.signal,
+        });
+        setIsDone(true);
+      } catch (e) {
+        if (e.code !== CODE) {
+          setIsError(true);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
 
-    return (
-      <div className="my-4">
-        <p>
-          {isDone && 'Success - request ended'}
-          {isError && 'Error - no data'}
-          {isLoading && 'Loading ...'}
-        </p>
+    return () => {
+      controllerRef.current.abort();
+    };
+  }, []);
 
-        <button className=" btn btn-secondary mt-4" type="button">
-          Reject
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="my-4">
+      <p>
+        {isDone && 'Success - request ended'}
+        {isError && 'Error - no data'}
+        {isLoading && 'Loading ...'}
+      </p>
+
+      <button
+        onClick={() => controllerRef.current.abort()}
+        className="btn btn-secondary mt-4"
+        type="button"
+      >
+        Reject
+      </button>
+    </div>
+  );
+};
+
+// class LongRequestPage extends Component {
+//   state = {
+//     isError: false,
+//     isLoading: false,
+//     isDone: false,
+//   };
+
+//   controller = new AbortController();
+
+//   async componentDidMount() {
+//     this.setState({ isLoading: true, isError: false });
+
+//     try {
+//       await axios.get('https://goit-fs.netlify.app/api/long', {
+//         signal: this.controller.signal,
+//       });
+//       this.setState({ isDone: true });
+//     } catch (e) {
+//       if (e.code !== CODE) {
+//         this.setState({ isError: true });
+//       }
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     this.controller.abort();
+//   }
+
+//   handleStopRequest = () => {
+//     this.controller.abort();
+//   };
+
+//   render() {
+//     const { isError, isLoading, isDone } = this.state;
+
+//     return (
+//       <div className="my-4">
+//         <p>
+//           {isDone && 'Success - request ended'}
+//           {isError && 'Error - no data'}
+//           {isLoading && 'Loading ...'}
+//         </p>
+
+//         <button
+//           onClick={this.handleStopRequest}
+//           className="btn btn-secondary mt-4"
+//           type="button"
+//         >
+//           Reject
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 
 export default LongRequestPage;
