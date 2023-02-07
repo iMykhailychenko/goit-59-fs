6195726b8a6cc7ec5013d26024d90762356cb158
imykhailychenko <igor.c.m@ukr.net>
2023-02-07 18:25:08 +0100

    lesson-13-redux-init

--- src/redux/store.js
+++ src/redux/store.js
@@ -19,29 +19,3 @@ const rootReducer = combineReducers({
 
 const enhancer = devToolsEnhancer();
 export const store = createStore(rootReducer, initState, enhancer);
-
-// const reducer = (state, action) => {
-//   switch (action.type) {
-//     case 'COUNTER':
-//       return { ...state, counter: state.counter + action.payload };
-
-//     default:
-//       return state;
-//   }
-
-//   //   if (action.type === 'PLUS') {
-//   //     // state.counter += 1;
-//   //     // return state;
-//   //     return { counter: state.counter + 1 };
-//   //   }
-
-//   //   if (action.type === 'MINUS') {
-//   //     return { counter: state.counter - 1 };
-//   //   }
-
-//   //   return state;
-// };
-
-// store.dispatch({ type: 'PLUS' });
-// store.dispatch({ type: 'PLUS' });
-// store.dispatch({ type: 'MINUS' });
