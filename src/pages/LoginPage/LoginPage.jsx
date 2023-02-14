85371240498bc45ada0001bdf2696abd7135584c
imykhailychenko <igor.c.m@ukr.net>
2023-02-14 22:15:33 +0100

    lesson-15

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import { STATUS } from '../../constants/status.constants';
